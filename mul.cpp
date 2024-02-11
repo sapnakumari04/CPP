ac1db002a89972ae1f08fe667813229a860d2096
Sapna Kumari <114881855+sapnakumari04@users.noreply.github.com>
2024-02-12 00:23:12 +0530

    Add files via upload
    This program is used to display the multiplication result of two number.

---
+++ mul.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+
+    // Ask the user to enter the first number
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    // Ask the user to enter the second number
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    // Multiplying  the two numbers
+    sum = num1 * num2;
+
+    // Displaying the result
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}
