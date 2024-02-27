153d2025431451231f503baaf2615b49c34b1dc4
Sapna Kumari <114881855+sapnakumari04@users.noreply.github.com>
2024-02-28 01:53:18 +0530

    Add files via upload

---
+++ seriesSum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int findsum(int num){
+    int sum=0;
+    for(int i=0;i<num;i++){
+        sum=sum+i;
+    }
+    return sum;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the value of n , till which sum is required:-";
+    cin>>n;
+    cout<<findsum(n);
+    return 0;
+}
