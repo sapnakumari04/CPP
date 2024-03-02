dc8ac891984965553d68f90050beccb9bcfd4ff3
Sapna Kumari <114881855+sapnakumari04@users.noreply.github.com>
2024-03-02 16:15:36 +0530

    Add files via upload

---
+++ arithmaticop.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int sum(int,int);
+int sub(int,int);
+int mul(int,int);
+int div(int,int);
+int rem;
+
+int main()
+{
+
+ int a,b,m,su,s,d;
+
+ cout<<"Enter Two Numbers : \n";
+ cin>>a>>b;
+
+ s=sum(a,b);
+ su=sub(a,b);
+ m=mul(a,b);
+ d=div(a,b);
+
+ cout<<"\nSum : = "<<s<<"\nSubtraction : = "<<su<<endl;
+ cout<<"\nMultiplication : = "<<m<<"\n Division : = "<<d<<endl;
+ return 0;
+}
+
+int sum(int a,int b)
+{
+ rem=a+b;
+ return(rem);
+}
+
+int sub(int a,int b)
+{
+ rem=a-b;
+return(rem);
+}
+
+int mul(int a,int b)
+{
+ rem=a*b;
+return(rem);
+}
+
+int div(int a,int b)
+{
+ rem=a/b;
+return(rem);
+}
