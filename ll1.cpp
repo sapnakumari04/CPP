575c199918dad7957faffc1ca06571f760ad802a
Sapna Kumari <114881855+sapnakumari04@users.noreply.github.com>
2024-03-17 23:50:21 +0530

    Add files via upload

---
+++ ll1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+//Traversal in singly linked list
+
+struct Node{
+    int data;
+    Node*next;
+    Node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+void printlist(Node*head)
+{
+    Node*curr=head;
+    while(curr!=NULL)
+    {
+        cout<<curr->data<<" ";
+        curr=curr->next;
+    }
+}
+
+int main()
+{
+    Node*head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    printlist(head);
+
+
+    return 0;
+}
