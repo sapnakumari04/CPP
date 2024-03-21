4fac51d127df8d0dfb70351a8c7b48ae56ec3a47
Sapna Kumari <114881855+sapnakumari04@users.noreply.github.com>
2024-03-21 23:33:50 +0530

    Add files via upload

---
+++ ll7.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+//Code to insert element at head 
+
+struct Node
+{
+    int data;
+    Node*next;
+    Node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+Node*insert(Node*head,int val)
+{
+    Node*temp=new Node(val);
+     temp->next = head; 
+    return temp;
+    
+}
+
+void printlist(Node *head)
+{
+    Node*temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+Node* createLinkedList() {
+    Node* head = NULL;
+    Node* tail = NULL;
+    char choice = 'y';
+
+    while (choice == 'y' || choice == 'Y') {
+        int data;
+        cout << "Enter data for the new node: ";
+        cin >> data;
+
+        Node* newNode = new Node(data);
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+
+        cout << "Do you want to continue (y/n)?: ";
+        cin >> choice;
+    }
+
+    return head;
+}
+
+
+int main()
+{
+    Node* head = createLinkedList();
+    cout << "Before insertion: ";
+    printlist(head);
+
+    int val;
+    cout << "\nEnter the value you want to insert at the head: ";
+    cin >> val;
+    head = insert(head, val);
+
+    cout << "After insertion: ";
+    printlist(head);
+
+
+    return 0;
+}
+
+   
